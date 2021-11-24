03d7b22a4043b8a81191c720a26e4616286bcbed
Piotr.Paszynski1 <Piotr.Paszynski1@pl.ey.com>
2021-11-24 14:56:24 +0100

    change to SCM job

--- pipelineJob.groovy
+++ pipelineJob.groovy
@@ -1,24 +1,39 @@
-pipeline {
-    agent {
-        label 'testslave'
-    }
-    stages {
-
-        stage('SCM') {
-            steps {
-                sh "git clone https://github.com/PioPaEY/gs-rest-service.git"
+//pipeline {
+//    agent {
+//        label 'testslave'
+//    }
+//    stages {
+//
+//        stage('SCM') {
+//            steps {
+//                sh "git clone https://github.com/PioPaEY/gs-rest-service.git"
+//            }
+//        }
+//
+//        stage('Build') {
+//            steps {
+//                sh 'cd gs-rest-service/complete && mvn spring-boot:run'
+//            }
+//        }
+//        stage('Test'){
+//            steps {
+//                echo 'Test'
+//            }
+//        }
+//    }
+//}
+pipelineJob('job-dsl-plugin') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/PioPaEY/gs-rest-service.git')
+                    }
+                    branch('*/main')
+                }
             }
-        }
-
-        stage('Build') {
-            steps {
-                sh 'cd gs-rest-service/complete && mvn spring-boot:run'
-            }
-        }
-        stage('Test'){
-            steps {
-                echo 'Test'
-            }
+            lightweight()
         }
     }
 }
