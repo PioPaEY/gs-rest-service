b4570fc5e1769bffb27cded5cfb4cc0f441a3e83
Piotr.Paszynski1 <Piotr.Paszynski1@pl.ey.com>
2021-11-26 13:07:38 +0100

    Add thing for Tomcat deploy

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -4,17 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
 public class RestServiceApplication extends SpringBootServletInitializer {
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(RestServiceApplication.class);
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Spring Boot application for show how to switch left performance tests";
     }
 
 }
