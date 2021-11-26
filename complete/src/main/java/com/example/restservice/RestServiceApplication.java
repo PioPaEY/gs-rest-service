015b6b587b3615c1230d3deb1db57f7ee4965d48
Piotr.Paszynski1 <Piotr.Paszynski1@pl.ey.com>
2021-11-26 13:13:42 +0100

    Add thing for Tomcat deploy

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
 public class RestServiceApplication extends SpringBootServletInitializer {
 
     @Override
