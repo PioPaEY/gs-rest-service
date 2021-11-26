c0a5e91ac4e20f492c036f96e5fbe6c8170e5212
Piotr.Paszynski1 <Piotr.Paszynski1@pl.ey.com>
2021-11-26 12:48:22 +0100

    Add thing for Tomcat deploy

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -3,6 +3,7 @@ package com.example.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Spring Boot application for show how to switch left performance tests";
+	}
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
